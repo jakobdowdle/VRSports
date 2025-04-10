ece8f3dfe2cf1003f746078c681dbf6d5517fd99
Jakob Dowdle <52472328+jakobdowdle@users.noreply.github.com>
2025-04-09 22:29:13 -0600

    code cleanup and minor visual update

--- Assets/Scripts/WristMenuBehaviour.cs
+++ Assets/Scripts/WristMenuBehaviour.cs
@@ -1,11 +1,9 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.XR.Interaction.Toolkit;
 
 public class WristMenuBehaviour : MonoBehaviour
 {
     [SerializeField] private GameObject _wristMenu;
-    private InputDevice _controller;
     public InputActionAsset inputActionAsset;
     private InputAction _openMenu;
 
@@ -14,16 +12,11 @@ public class WristMenuBehaviour : MonoBehaviour
     }
 
     void Update() {
-        if (_openMenu.triggered)
-        {
-            //Debug.Log(_wristMenu.activeSelf);
-            ToggleWristMenu();
-        }
-        //Debug.Log(_wristMenu.activeSelf);
+        if (_openMenu.triggered) ToggleWristMenu();
 
     }
+
     private void ToggleWristMenu() {
-        //Debug.Log(_wristMenu.activeSelf);
         _wristMenu.SetActive(!_wristMenu.activeSelf);
 
     }
