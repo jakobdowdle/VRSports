f5b6786eb194849d6397e404c7f05d817c674a78
Jakob Dowdle <52472328+jakobdowdle@users.noreply.github.com>
2025-04-14 01:12:57 -0600

    WIP update wrist menu text with current information

--- Assets/Scripts/WristMenuBehaviour.cs
+++ Assets/Scripts/WristMenuBehaviour.cs
@@ -1,9 +1,15 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class WristMenuBehaviour : MonoBehaviour
 {
     [SerializeField] private GameObject _wristMenu;
+    [SerializeField] private TextMeshProUGUI _holeText;
+    [SerializeField] private TextMeshProUGUI _parText;
+    [SerializeField] private TextMeshProUGUI _distanceText;
+    [SerializeField] private TextMeshProUGUI _scoreText;
+
     public InputActionAsset inputActionAsset;
     private InputAction _openMenu;
 
@@ -13,11 +19,14 @@ public class WristMenuBehaviour : MonoBehaviour
 
     void Update() {
         if (_openMenu.triggered) ToggleWristMenu();
-
     }
 
     private void ToggleWristMenu() {
         _wristMenu.SetActive(!_wristMenu.activeSelf);
 
+    }
+
+    public void UpdateMenu() {
+        _holeText.text = "Hole " + 2;
     }
 }
